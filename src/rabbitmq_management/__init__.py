9c221c89932c6d637b435161f006c2b8f8f59591
Rivkat Karimov <redscorpion32.rk@gmail.com>
2026-04-06 02:12:32 +0500

    docs: add mkdocs

---
+++ src/rabbitmq_management/__init__.py
@@ -0,0 +1,4 @@
+from .management_api._async.management_api import AsyncRMQManagementAPI
+from .management_api._sync.management_api import RMQManagementAPI
+
+__all__ = ["AsyncRMQManagementAPI", "RMQManagementAPI"]
